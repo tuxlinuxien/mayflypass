b9c8d0a1fb725324e208d9edda55b37fb582e45c
Yoann Cerda <yoann.cerda@proton.me>
2026-06-03 22:28:38 +0800

    Update api.

--- api/target/debug/deps/api-5feea33faf16b2da.d
+++
@@ -1,5 +0,0 @@
-/home/yoann/work/mayflypass/api/target/debug/deps/api-5feea33faf16b2da.d: src/main.rs
-
-/home/yoann/work/mayflypass/api/target/debug/deps/libapi-5feea33faf16b2da.rmeta: src/main.rs
-
-src/main.rs:
--- api/target/debug/deps/api-ea8886fc8ef8e33f.d
+++
@@ -1,5 +0,0 @@
-/home/yoann/work/mayflypass/api/target/debug/deps/api-ea8886fc8ef8e33f.d: src/main.rs
-
-/home/yoann/work/mayflypass/api/target/debug/deps/libapi-ea8886fc8ef8e33f.rmeta: src/main.rs
-
-src/main.rs:
