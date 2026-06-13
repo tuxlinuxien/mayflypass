69ba1ba7a1c6b2b043ac018cdfbd769971ea0062
Yoann Cerda <yoann.cerda@proton.me>
2026-06-13 22:48:15 +0800

    Add deps to the flutter app.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mayflypass/core/theme.dart';
+import 'package:mayflypass/core/core.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
