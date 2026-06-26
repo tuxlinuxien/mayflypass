a741d166031ba60844fa3a6d6b88a2699152cf52
Yoann Cerda <yoann.cerda@proton.me>
2026-06-26 17:58:35 +0800

    Remove old comments.

--- app/lib/secure/constants.dart
+++ app/lib/secure/constants.dart
@@ -7,9 +7,9 @@ import 'package:cryptography_plus/cryptography_plus.dart';
 final secretKeyLength = Xchacha20.poly1305Aead().secretKeyLength;
 
 final authSalt = utf8.encode('domain/derive/auth');
-final authOuputLen = secretKeyLength; // into bits
+final authOuputLen = secretKeyLength;
 final kekSalt = utf8.encode('domain/derive/kek');
-final kekOuputLen = secretKeyLength; // into bits
+final kekOuputLen = secretKeyLength;
 
 final masterPasswordMemCost = 64 * 1024;
 final masterPasswordICost = 15;
