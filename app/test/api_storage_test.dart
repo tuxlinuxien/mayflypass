00fb97bca01c574f07960bb9b9ddccb93e53a56f
Yoann Cerda <yoann.cerda@proton.me>
2026-06-27 16:30:58 +0800

    app: Update interceptors.

---
+++ app/test/api_storage_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mayflypass/api/api.dart';
+import 'api_auth_test.dart';
+
+void main() {
+  group('api storage', () {
+    test('upsert', () async {
+      await setupAccount();
+      final input = Storage.create(
+        encryptedDek: Uint8List(0),
+        encryptedPayload: Uint8List(0),
+      );
+      final response = await API().storageUpsert(input);
+      expect(response.id, input.id);
+      expect(response.createdAt, input.createdAt);
+      expect(response.updatedAt, input.updatedAt);
+      expect(response.version, input.version);
+      expect(response.deleted, input.deleted);
+      expect(response.encryptedDek, input.encryptedDek);
+      expect(response.encryptedPayload, input.encryptedPayload);
+    });
+  });
+}
