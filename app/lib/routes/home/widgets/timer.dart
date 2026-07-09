c614765e4ac0223802ec4782e1c58957288671e0
Yoann Cerda <yoann.cerda@proton.me>
2026-07-09 22:14:22 +0800

    app: Fix lint errors.

--- app/lib/routes/home/widgets/timer.dart
+++ app/lib/routes/home/widgets/timer.dart
@@ -1,7 +1,5 @@
 import 'package:mayflypass/core/core.dart';
-import 'package:mayflypass/databox/databox.dart';
 import 'package:mayflypass/routes/home/widgets/epoch_cubit.dart';
-import 'package:otp/otp.dart';
 
 class Timer extends StatelessWidget {
   final int period;
@@ -31,53 +29,5 @@ class Timer extends StatelessWidget {
   double _timeLeft(int state) {
     final progress = _endPeriod(state) - state;
     return progress / (period * 1000);
-  }
-}
-
-class Code extends StatelessWidget {
-  final String secret;
-  final TotpAlgorithm algorithm;
-  final int period;
-  final int digits;
-
-  const Code({
-    super.key,
-    required this.secret,
-    required this.algorithm,
-    required this.digits,
-    required this.period,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => EpochCubit(),
-      child: BlocBuilder<EpochCubit, int>(
-        builder: (context, state) {
-          return Text(
-            _genCode(state),
-            style: Theme.of(context).textTheme.headlineMedium,
-          );
-        },
-      ),
-    );
-  }
-
-  String _genCode(int state) {
-    final algo = switch (algorithm) {
-      .SHA1 => Algorithm.SHA1,
-      .SHA256 => Algorithm.SHA256,
-      .SHA512 => Algorithm.SHA512,
-      _ => throw UnimplementedError(),
-    };
-
-    return OTP.generateTOTPCodeString(
-      secret,
-      state,
-      algorithm: algo,
-      interval: period,
-      length: digits,
-      isGoogle: true,
-    );
   }
 }
