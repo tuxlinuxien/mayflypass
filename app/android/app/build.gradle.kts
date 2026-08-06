e5bf9aa499a8827e27a8a957741ff9eb5ad09623
Yoann Cerda <yoann.cerda@proton.me>
2026-08-06 23:35:23 +0800

    app: Update compileSdk version.

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mayflypass.mayflypass"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
